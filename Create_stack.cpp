96a9ab145db29da965b232548a37821b4f27d884
apnacollage.demo <dhananjaymewada62@gamil.com>
2026-06-21 01:08:34 +0530

    new file

---
+++ Create_stack.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*next;
+    node(int value){
+        data=value;
+        next=NULL;
+
+    }
+};
+class stack{
+    node*top;
+    int size;
+    public:
+    stack(){
+        top=NULL;
+        size=0;
+    }
+    void push(int value){
+        node*temp=new node(value);
+        if(temp==NULL){
+            cout<<"stack overflow";
+            return;
+        }
+        else{
+        temp->next=top;
+        top=temp;
+        size++;
+       cout<<"pused= "<<value<<endl;
+    }
+}
+void pop(){
+    if(top==NULL){
+   cout<<"stack underflow";
+
+
+    }
+    else{
+        node*temp=top;
+         cout<<"delete the node="<<top->data<<endl;
+        top=top->next;
+        delete temp;
+       
+    }
+}
+int peek(){
+    if(top==NULL){
+        return -1;
+    }
+    else{
+        return top->data;
+       
+    
+    }
+}
+ 
+    
+};
+
+int main(){
+stack s;
+s.push(5);
+s.push(2);
+s.push(1);
+s.push(0);
+s.pop();
+cout<<s.peek();
+
+
+return 0;
+}
